94ce4a78d9df572a2550fe61e3d750d962672f41
odilova <odilovahilola27@gmail.com>
2023-12-05 14:35:51 +0500

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+
+    },
+    getters: {
+
+    },
+    actions: {
+
+    },
+    mutations: {
+
+    },
+})
+
+export default store;
